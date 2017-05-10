97aabe55a38fc54fc55b95dca6a804fe70484660
TerryD <lord_silver_fox@hotmail.com>
2017-05-10 22:24:33 +1000

    more command parser

--- src/j_validate/J_Validate.java
+++ src/j_validate/J_Validate.java
@@ -87,7 +87,18 @@ public class J_Validate
             {
                System.out.println(ex.toString());
             }
-            JDisplayListofArgs(ParserSettingsObject.strSysType);
+            JParseArguments(args, ParserSettingsObject);
+            // execute validation check of applied settings
+            // begin parsering of files
+            try 
+            {
+                FileProcessorMain objFileProcess = new FileProcessorMain(ParserSettingsObject, Logger);
+                objFileProcess.FileProcessorRun();
+            }
+            catch (Exception ex)
+            {
+                System.out.println("Error: " + ex.getMessage());
+            }
             System.exit(0);
             
         }
@@ -133,8 +144,123 @@ public class J_Validate
 
     }
     
+    public static boolean JIsArgValidReturnBool(String _strArg)
+    {
+        boolean _bOutput = false;
+            switch(_strArg.toUpperCase())
+            {
+                case "-I":
+                    _bOutput = true;
+                    break;
+                    
+                case "-O":
+                    _bOutput = true;
+                    break;
+                    
+                case "-E":
+                    _bOutput = true;
+                    break;
+                    
+                case "-M":
+                    _bOutput = true;
+                    break;
+                    
+                case "-S":
+                    _bOutput = true;
+                    break;
+                    
+                case "-?":
+                    _bOutput = true;
+                    break;
+                    
+                    
+                default:
+                    _bOutput = false;
+                    break;
+            }
+        return _bOutput;
+    }
     
+    public static void JParseArguments(String[] _argtemp, parser_settings _settingObj)
+    {
+        for (int numArg = 0; numArg < (_argtemp.length - 1) ; numArg++) 
+        {
+            switch(_argtemp[numArg].toUpperCase())
+            {
+                case "-I":
+                    numArg++;
+                    if (!JIsArgValidReturnBool(_argtemp[numArg]))
+                    {
+                        _settingObj.strInputPath = _argtemp[numArg];
+                    }
+                    break;
+                    
+                case "-O":
+                    numArg++;
+                    if (!JIsArgValidReturnBool(_argtemp[numArg]))
+                    {
+                        _settingObj.strOutputPath = _argtemp[numArg];
+                    }
+                    break;
+                    
+                case "-E":
+                    numArg++;
+                    if (!JIsArgValidReturnBool(_argtemp[numArg]))
+                    {
+                        _settingObj.strExtractPath = _argtemp[numArg];
+                    }
+                    break;
+                    
+                case "-M":
+                    numArg++;
+                    if (!JIsArgValidReturnBool(_argtemp[numArg]))
+                    {
+                        _settingObj.strFileValidationMask = _argtemp[numArg];
+                    }
+                    break;
+                    
+                case "-S":
+                    if (_argtemp.length >= 2)
+                    {
+                        System.out.println("-s must be the only command in use ");
+                    }
+                    else
+                    {
+                        numArg++;
+                        if (!JIsArgValidReturnBool(_argtemp[numArg]))
+                        {
+                            _settingObj.strSettingsFile = _argtemp[numArg];
+                        }
 
+                    }
+                    break;
+                    
+                case "-?":
+                    JDisplayListofArgs(_settingObj.strSysType);
+                    System.exit(0);
+                    break;
+                    
+                    
+                default:
+                    System.out.println("Error detected in command - argument: " + _argtemp[numArg]);
+                    break;
+            }
+        }
+    }
+    
+    public static void JValidateSettings(parser_settings _settingObj)
+    {
+        // validate paths as valid or files can be accessed / found
+    }
+    
+    public static void JParseSettingsFile(parser_settings _settingObj)
+    {
+        // parse settings from file
+        // validate settings
+    }
+    
+    
+/*
     public void JParseArguments(String[] _argtemp, parser_settings _settingObj)
     {
         
@@ -142,6 +268,8 @@ public class J_Validate
         String strPrequelCommand = "";
         String strErrorResponse = "";
         boolean bErrorFlag = false;
+        
+        
         
         CommandLinkedList CommandParams = new CommandLinkedList();
         for (int numArg = 0; numArg < (_argtemp.length - 1) ; numArg++) 
@@ -154,95 +282,55 @@ public class J_Validate
             if (CommandParams.FindCommandInListReturnValid(_argtemp[numArg]))
                     {
                         int numComCount = CommandParams.FindCommandInListReturnParams(_argtemp[numArg]);
-                        for (int i = 0; i < (numComCount -1); i++) 
-                        {
-                            
+                        strErrorResponse = _argtemp[numArg];
+                        if (numComCount > 0 )
+                        {    
+                            // Commands here have Params
+                            for (int i = 0; i < (numComCount -1); i++) 
+                            {
+                                numArg++;
+                                if (CommandParams.FindCommandInListReturnValid(_argtemp[numArg]))
+                                {
+                                    bErrorFlag = true;
+                                    strErrorResponse = strErrorResponse + _argtemp[numArg];
+                                    break;
+                                    //throw error params should not be valid here
+                                }
+                                else
+                                {
+
+                                }
+                            }
                         }
+                        else
+                        {
+                            // handle switches here.
+                        }
+                            
+                        
                     }
             else
             {
                 // invalid command
-            }
-/*            
-            if  ("-I".equals(_argtemp[numArg].toUpperCase()))
-            {
+                bErrorFlag = true;
                 
-                _settingObj.strInputPath = _argtemp[numArg];
+            }
+
+            if (bErrorFlag)
+            {
+                String strErrorLine = "Error in: " + strErrorResponse;
+                System.out.println(strErrorLine);
+                break;
             }
             
-            else if  ("-O".equals(_argtemp[numArg].toUpperCase()))
-            {
-                _settingObj.strOutputPath = _argtemp[numArg];
-            }
             
-            else if  ("-M".equals(_argtemp[numArg].toUpperCase()))
-            {
-                _settingObj.strFileValidationMask = _argtemp[numArg];
-            }
-                
-            else if  ("-E".equals(_argtemp[numArg].toUpperCase()))
-            {
-                _settingObj.strExtractPath = _argtemp[numArg];
-            }
             
-            else if  ("-S".equals(_argtemp[numArg].toUpperCase()))
-            {
-                
-            }    
-            
-            else if  ("-?".equals(_argtemp[numArg].toUpperCase()))
-            {
-                
-            }    
-            else
-            {
-                //invalid command
-            }
-  */             
         }
         
         
             
         
     }
-    
-    public boolean TestValidCommand(String _strTempCommand)
-    {
-        boolean bValidCommand = false;
-    
-            switch (_strTempCommand.toUpperCase())
-                {
-                    case "-I":
-                      bValidCommand = true;
-                    break;
-                        
-                    case "-O":
-                        bValidCommand = true;
-                        break;
-                        
-                    case "-M":
-                        bValidCommand = true;
-                        break;
-                        
-                    case "-E":
-                        bValidCommand = true;
-                        break;
-                    
-                    case "-?":
-                         bValidCommand = true;
-                        //JDisplayListofArgs();
-                        //System.exit(0);
-                        break;
-                    
-                    case "-S":
-                        // check for len, if more then 2 commands (S switch + filepath) then exit with error
-                         bValidCommand = true;
-                    break;
-                    default:
-                         bValidCommand = false;
-                        break;
-                }
-            return  bValidCommand;
-    }
+  */  
     
 }
